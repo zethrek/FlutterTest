b06e632f1afb1fb234c4422a27a9c5583f38e0c8
luis paredes <luis.paredes.y@gmail.com>
2021-05-18 18:17:06 -0400

    assets agregados

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset(
+            'assets/logo.png',
+            height: 50,
+            )
+        ],
+      ),
+    );
+  }
+} 
+
